6cc6514a1cf77a6a48968a8dc1f3757c0e70e7ff
Cristobal <cristobal.medrano@usach.cl>
2020-06-14 15:45:28 -0400

    Base program

---
+++ Medrano.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+#AUTHOR: Cristóbal Nicolás Medrano Alvarado (19.083.864-1)
+#DATE: 19/06/2020
+#LABORATORY 2: CONVOLUCIÓN 2D
+
+# IMPORTS
+
+# CONSTANTS 
+# GLOBAL VARIABLES
+# CLASSES
+# FUNCTIONS
+def main():
+    print("Laboratorio 2 - Convolución 2D")
+
+# MAIN
+main()
